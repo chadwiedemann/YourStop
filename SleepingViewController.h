cffdd0abe02890f7ce79d939a35c84826d9db823
Chutima <chutima.mungmee@gmail.com>
2016-11-29 13:56:32 -0500

    untracking file

--- SleepingViewController.h
+++ SleepingViewController.h
@@ -9,12 +9,22 @@
 #import <UIKit/UIKit.h>
 #import "Destination.h"
 #import <UserNotifications/UserNotifications.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface SleepingViewController : UIViewController
+@interface SleepingViewController : UIViewController <CLLocationManagerDelegate, CLLocationManagerDelegate, UIAlertViewDelegate>
 
+@property (weak, nonatomic) IBOutlet UILabel *testingLabel;
 @property (nonatomic, strong) Destination *destination;
 @property double distanceInMeters;
 @property (nonatomic, strong) UNMutableNotificationContent *content;
 @property (nonatomic, strong) UNTimeIntervalNotificationTrigger *trigger;
 @property (nonatomic, strong) UNUserNotificationCenter *center;
+@property (nonatomic, strong) UNTimeIntervalNotificationTrigger *timeTrigger;
+@property (nonatomic, strong) CLLocationManager *locationManager;
+
+//uilocalnotification section
+
+
+@property int checked;
+
 @end
