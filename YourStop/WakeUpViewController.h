8567e78c9a83c0c569245f456c0a0d4f079e23b3
Chutima <chutima.mungmee@gmail.com>
2016-12-01 11:21:41 -0500

    add wake up viewcontroller

---
+++ YourStop/WakeUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  WakeUpViewController.h
+//  YourStop
+//
+//  Created by chutima mungmee on 11/30/16.
+//  Copyright © 2016 Chad Wiedemann LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WakeUpViewController : UIViewController
+
+@end
